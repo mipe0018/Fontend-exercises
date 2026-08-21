103ca5c08d267fe0e3449b09a710e88de9d2af1e
mipe0018 <mipe0018@stud.ek.dk>
2026-08-21 11:04:10 +0200

    import module from utils_lib to main.js

---
+++ tilfældige tal/js/main.js
@@ -0,0 +1,5 @@
+import { generateRandomNumberBetween0And100 } from "../utils_lib/utils_lib.js";
+
+const randomNumber = generateRandomNumberBetween0And100(100);
+
+console.log(randomNumber);
---
+++ tilfældige tal/utils_lib/utils_lib.js
@@ -0,0 +1,6 @@
+  //retunere et tilfældigt tal mellem 0 - 100 
+ 
+ export function generateRandomNumberBetween0And100(max) {
+  return Math.floor(Math.random() * max)
+ };
+
