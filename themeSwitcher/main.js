8f5ae583e9bb79817419559819ed6163bbb6e28a
mipe0018 <mipe0018@stud.ek.dk>
2026-09-01 03:26:44 +0200

    themeSwitcher js.error in github only

--- themeSwitcher/main.js
+++ themeSwitcher/main.js
@@ -1,14 +1,11 @@
 "use strict";
 
-// Det rigtige element her:
-const themeSelect = document.getElementById('theme-select');
+const themeSelect = document.getElementById("theme-select");
 
-// Funktionen der ændrer farvetema:
 function optionChoice() {
     const choice = themeSelect.value;
 
-    // Nulstil altid klassen på body først, så temaerne ikke blandes sammen
-    document.body.className = '';
+    document.body.className = "";
 
     if (choice === "light") {
         document.body.classList.add("light-theme");
@@ -19,5 +16,27 @@ function optionChoice() {
     }
 }
 
-// Ændrer 'select' til 'themeSelect', så det matcher variabelen i toppen
 themeSelect.addEventListener("change", optionChoice);
+//"use strict";
+
+// Det rigtige element her:
+//const themeSelect = document.getElementById//('theme-select');
+
+// Funktionen der ændrer farvetema:
+//function optionChoice() {
+//  const choice = themeSelect.value;
+
+    // Nulstil altid klassen på body først, så temaerne ikke blandes sammen
+//    document.body.className = '';
+
+//    if (choice === "light") {
+//        document.body.classList.add("light-theme");
+//    } else if (choice === "pink") {
+//        document.body.classList.add("pink-theme");
+//    } else if (choice === "dark") {
+//        document.body.classList.add("dark-theme");
+//    }
+//}
+
+// Ændrer 'select' til 'themeSelect', så det matcher variabelen i toppen
+//themeSelect.addEventListener("change", optionChoice);
