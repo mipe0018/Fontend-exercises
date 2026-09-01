6357d51c13de558b55c08ea9ff82e776b2374eaa
mipe0018 <mipe0018@stud.ek.dk>
2026-09-01 18:51:51 +0200

    ekstra themes added

--- themeSwitcher/main.js
+++ themeSwitcher/main.js
@@ -18,30 +18,14 @@ function optionChoice() {
     } else if (choice === "dark") {
         document.body.classList.add("dark-theme");
     }
+    else if (choice === "mocha") {
+        document.body.classList.add("mocha-them");
+    }
+    else if (choice === "pastel") {
+        document.body.classList.add("pastel-theme");
+    } else if (choice === "cloud") {
+        document.body.classList.add("cloud-theme");
+    }
 }
 
 themeSelect.addEventListener("change", optionChoice);
-//"use strict";
-
-// Det rigtige element her:
-//const themeSelect = document.getElementById//('theme-select');
-
-// Funktionen der ændrer farvetema:
-//function optionChoice() {
-//  const choice = themeSelect.value;
-
-    // Nulstil altid klassen på body først, så temaerne ikke blandes sammen
-//    document.body.className = '';
-
-//    if (choice === "light") {
-//        document.body.classList.add("light-theme");
-//    } else if (choice === "pink") {
-//        document.body.classList.add("pink-theme");
-//    } else if (choice === "dark") {
-//        document.body.classList.add("dark-theme");
-//    }
-//}
-
-// Ændrer 'select' til 'themeSelect', så det matcher variabelen i toppen
-//themeSelect.addEventListener("change", optionChoice);
-
