0ade7b177c0577920473243e3d7a2fe811f02799
mipe0018 <mipe0018@stud.ek.dk>
2026-09-01 02:56:51 +0200

    theme-switcher css + html + themes + js start-up

---
+++ themeSwitcher/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const lightTheme = document.querySelector("#light-theme");
+const pinkTheme = document.querySelector("#pink-theme");
+const darkTheme = document.querySelector("#dark-theme");
+
+function optionChoice {
+      const choice = select.value;
+
+    if (choice === "light") {
+        // Gør noget
+    } else if (choice === "pink") {
+        // Gør noget andet
+    } else if (choice === "dark") {
+        // Gør noget tredje
+    }
+}
+
+select.addEventListener("change", optionChoice);
